94074290e696da2e0fcd48253056d8b9789cd502
Dmitriy Ivashchenko <dmitriy.ivashchenko.work@gmail.com>
2018-11-20 15:55:45 +0200

    add one backpack and search (brute force) method

--- src/com/divashchenko/Main.java
+++ src/com/divashchenko/Main.java
@@ -1,8 +1,37 @@
 package com.divashchenko;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<Object> allObjects = new ArrayList<>();
 
+        Object object1 = new Object(10, 5);
+        Object object2 = new Object(20, 29);
+        Object object3 = new Object(5, 8);
+        Object object4 = new Object(7, 12);
+        Object object5 = new Object(12, 16);
+        Object object6 = new Object(2, 8);
+        Object object7 = new Object(24, 1);
+        Object object8 = new Object(4, 10);
+        Object object9 = new Object(2, 9);
+
+        allObjects.add(object1);
+        allObjects.add(object2);
+        allObjects.add(object3);
+        allObjects.add(object4);
+        allObjects.add(object5);
+        allObjects.add(object6);
+        allObjects.add(object7);
+        allObjects.add(object8);
+        allObjects.add(object9);
+
+        Backpack backpack = new Backpack(17);
+
+        System.out.println("All objects: " + allObjects);
+        System.out.println();
+        Sorter.sort(backpack, allObjects);
     }
 }
--- src/com/divashchenko/Sorter.java
+++ src/com/divashchenko/Sorter.java
@@ -1,5 +1,72 @@
 package com.divashchenko;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sorter {
-    
+    private static List<Object> bestSet;
+    private static int maxWeight;
+    private static int maxPrice;
+
+    public List<Object> getBestSet() {
+        return bestSet;
+    }
+
+    public static void sort(Backpack backpack, List<Object> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getWeight() > backpack.getMaxWeight()) {
+                list.remove(i);
+                i--;
+            }
+        }
+
+        maxWeight = backpack.getMaxWeight();
+
+        startSort(list);
+
+        System.out.println("Result: " + bestSet);
+
+    }
+
+    public static void startSort(List<Object> list) {
+        if (list.size() > 0) {
+            reviewSet(list);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            List<Object> tmpSet = new ArrayList<>(list);
+            tmpSet.remove(i);
+            startSort(tmpSet);
+        }
+    }
+
+    private static void reviewSet(List<Object> list) {
+        if (bestSet == null) {
+            if (sumWeight(list) <= maxWeight) {
+                bestSet = list;
+                maxPrice = sumPrice(list);
+            }
+        } else {
+            if (sumWeight(list) <= maxWeight && sumPrice(list) > maxPrice) {
+                bestSet = list;
+                maxPrice = sumPrice(list);
+            }
+        }
+    }
+
+    private static int sumWeight(List<Object> list) {
+        int sumW = 0;
+        for (Object aList : list) {
+            sumW += aList.getWeight();
+        }
+        return sumW;
+    }
+
+    private static int sumPrice(List<Object> list) {
+        int sumP = 0;
+        for (Object aList : list) {
+            sumP += aList.getPrice();
+        }
+        return sumP;
+    }
 }
